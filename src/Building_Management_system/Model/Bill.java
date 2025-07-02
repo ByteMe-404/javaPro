e92b582d1b2d5a701cbc415548e268dcd551c590
ByteMe-404 <rian.official444@gmail.com>
2025-07-02 09:56:53 +0600

    model classed

---
+++ src/Building_Management_system/Model/Bill.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Building_Management_system.Model;
+
+/**
+ *
+ * @author rian64bit
+ */
+public class Bill {
+    
+}
